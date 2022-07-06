edea70838d9b6830894d1624cce3d27d62d27c23
KeithFabre <keithfmacedo@poli.ufrj.br>
2022-07-05 22:53:48 -0300

    finished functions for album-info route

--- api/utils.py
+++ api/utils.py
@@ -30,7 +30,7 @@ def ageCalc(birthday, date=today):
     
     return years
 
-def getArtistId(artist_name):
+def getArtistInfo(artist_name):
     
     APIKEY = 2 # API KEY for tests 
 
@@ -39,8 +39,9 @@ def getArtistId(artist_name):
     response = requests.get(url)
 
     id = response.json()['artists'][0]['idArtist']
+    name = response.json()['artists'][0]['strArtist']
 
-    return id
+    return id, name
 
 def getArtistLatestAlbum(artist_id):
 
@@ -81,9 +82,12 @@ def getAlbumTracks(album_latest):
 
     APIKEY = 2 # API KEY for tests 
 
+    # get all ids for the albums from the latest year
     ids = [a['id'] for a in album_latest]
 
-    # if more than one id for one year check if there are singles
+    # if there is more than one id for one year check if there are singles
+    
+    # more than one id
     if len(ids) > 1:
         for id in ids:
             url = "https://theaudiodb.com/api/v1/json/{}/track.php?m={}".format(APIKEY, id)
@@ -92,35 +96,60 @@ def getAlbumTracks(album_latest):
 
             tracks = response.json()['track']
 
-            # check if its a single
+            # its a single (only one track) 
             if len(tracks) == 1:
                 full_album = None
             
-            # if not a single then store the latest album
+            # not a single (more than on track) then store latest album
             elif len(tracks) > 1:
                 full_album = tracks # not a single, full album
+                album_id = id # store album id
 
-    # only one id -- no further if-statement necessary
+    # only one id -- get the only one
     else:
         id = ids[0]
+        
+        album_id = id # store album id
+
         url = "https://theaudiodb.com/api/v1/json/{}/track.php?m={}".format(APIKEY, id)
 
         response = requests.get(url)
 
-        tracks = response.json()['track']
+        full_album = response.json()['track']
+
     
     # create dict with number and name of every track
+    i = 1
+    album_tracks = {}
+    for track in full_album:
+        key = str(i)
+        album_tracks[key] = track["strTrack"]
+        i+=1
+    
+    return album_tracks, album_id
 
-    return tracks
 
+artist_name = "imagine dragons"
 
-artist_name = "coldplay"
+# artist id
+art_id  = getArtistInfo(artist_name)[0]
+artist_name = getArtistInfo(artist_name)[1]
 
-art_id  = getArtistId(artist_name)
+# year of latest albums
+album_year = getArtistLatestAlbum(art_id)[0] 
 
-album_year = getArtistLatestAlbum(art_id)[0]
-album_latest = getArtistLatestAlbum(art_id)[1]
+# album id and name from year of latest release
+album_latest = getArtistLatestAlbum(art_id)[1] 
 
-album_tracks = getAlbumTracks(album_latest)
+# tracks from lastest album
+album_tracks = getAlbumTracks(album_latest)[0]
+# album id of latest album with described tracks
+album_id = getAlbumTracks(album_latest)[1]
+# album name of latest album with described tracks
+album_name = [a['name'] for a in album_latest if a['id'] == album_id][0]
+
+print(artist_name) 
+print(album_name)
+print(album_year)
 print(album_tracks)
 
